16b3d290e572c5b0dbe6abd737ed49f453fde3c2
Eray Kısabacak <nefret19981998@hotmail.com>
2019-11-17 23:12:31 +0300

    Gibson Knight Yonetimi

---
+++ BilgisayarMimarisi/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgisayarMimarisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mov = { "0.2", "2" };
+            string[] mul = { "0.4", "5" };
+            string[] add = { "0.3", "3" };
+            string[] adc = { "0.3", "3" };
+
+            int movSayisi = 0;
+            int mulSayisi = 0;
+            int addSayisi = 0;
+            int adcSayisi = 0;
+            
+
+
+            string dosya_yolu = "../../soru1.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string yazi = sr.ReadLine();
+            while(yazi != null)
+            {
+                string[] icerik = yazi.Split(' ');
+                if (icerik[0].Equals("MOV"))
+                {
+                    movSayisi++;
+                }
+                else if (icerik[0].Equals("MUL"))
+                {
+                    mulSayisi++;
+                }
+                else if (icerik[0].Equals("ADD"))
+                {
+                    addSayisi++;
+                }
+                else if (icerik[0].Equals("ADC"))
+                {
+                    adcSayisi++;
+                }
+                yazi = sr.ReadLine();
+            }
+
+            sr.Close();
+            fs.Close();
+            for(double i = 1; i <= 5; i += 0.1)
+            {
+                double t = 1 / i * Math.Pow(10, 9);
+                double sonuc = ((movSayisi * double.Parse(mov[0])) + 
+                                (mulSayisi * double.Parse(mul[0])) + 
+                                (addSayisi * double.Parse(add[0])) + 
+                                (adcSayisi * double.Parse(adc[0])))
+                    /
+                    ((movSayisi * double.Parse(mov[0]) * int.Parse(mov[1]) * t) + 
+                    (mulSayisi * double.Parse(mul[0]) * int.Parse(mul[1]) * t) + 
+                    (addSayisi * double.Parse(add[0]) * int.Parse(add[1]) * t) + 
+                    (adcSayisi * double.Parse(adc[0]) * int.Parse(adc[1]) * t));
+                Console.WriteLine(i + " ** " + sonuc);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ BilgisayarMimarisiSoru2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgisayarMimarisiSoru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int movSayisi = 0;
+            int mulSayisi = 0;
+            int addSayisi = 0;
+            int adcSayisi = 0;
+            int incSayisi = 0;
+            int cmpSayisi = 0;
+
+            int movCLK = 2;
+            int mulCLK = 4;
+            int addCLK = 2;
+            int adcCLK = 2;
+            int incCLK = 2;
+            int cmpCLK = 3;
+
+
+            string dosya_yolu = "../../soru2.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string yazi = sr.ReadLine();
+            while (yazi != null)
+            {
+                string[] icerik = yazi.Split(' ');
+                if (icerik[0].Equals("MOV"))
+                {
+                    movSayisi++;
+                }
+                else if (icerik[0].Equals("MUL"))
+                {
+                    mulSayisi++;
+                }
+                else if (icerik[0].Equals("ADD"))
+                {
+                    addSayisi++;
+                }
+                else if (icerik[0].Equals("ADC"))
+                {
+                    adcSayisi++;
+                }
+                else if (icerik[0].Equals("INC"))
+                {
+                    incSayisi++;
+                }
+                else if (icerik[0].Equals("CMP"))
+                {
+                    cmpSayisi++;
+                }
+
+                yazi = sr.ReadLine();
+            }
+            sr.Close();
+            fs.Close();
+
+            int komut = (movSayisi + mulSayisi + addSayisi + adcSayisi + incSayisi + cmpSayisi);
+            int clk = ( movSayisi * movCLK + 
+                        mulSayisi * mulCLK + 
+                        addSayisi * addCLK + 
+                        adcSayisi * adcCLK + 
+                        incSayisi * incCLK + 
+                        cmpSayisi * cmpCLK);
+
+            double tort = clk / komut;
+
+            for (double i = 1; i <= 5; i += 0.1)
+            {
+                double t = 1 / i * Math.Pow(10, 9);
+                Console.WriteLine("***************************************");
+                Console.WriteLine("T ortalama Komut " + (tort * t));
+                Console.WriteLine("V " + (1 / (tort * t)));
+                Console.WriteLine("***************************************");
+
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
